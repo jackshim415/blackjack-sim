855c895d9d8d297af5d3712e4b5561503a51ae62
JackAttack-365 <142643773+jackattack-4@users.noreply.github.com>
2026-09-05 16:34:51 -0700

    added armoir()

--- src/strategy.rs
+++ src/strategy.rs
@@ -1,7 +1,8 @@
 use crate::util::{Action, Hand};
 
 pub fn strategy(hand: &Hand, dealer_upcard: u8) -> Action {
-    over(hand,dealer_upcard,16)
+    armoir(hand, dealer_upcard)
+    //over(hand,dealer_upcard,16)
 }
 
 pub fn base_strategy(hand: &Hand, dealer_upcard: u8) -> Action {
@@ -17,5 +18,19 @@ fn over(hand: &Hand, dealer_upcard: u8, over: u16) -> Action {
         Action::Hit
     } else {
         Action::Stand
+    }
+}
+
+fn armoir(hand: &Hand, dealer_upcard: u8) -> Action {
+    const FOURFIVESIX: [u8; 3] = [4, 5, 6];
+    match hand.count {
+        17.. => Action::Stand,
+        13..=16 => {if dealer_upcard < 7 {
+            Action::Stand
+        } else {Action::Hit}},
+        12 => {if FOURFIVESIX.contains(&dealer_upcard) {
+            Action::Stand
+        } else {Action::Hit}},
+        ..=11 => Action::Hit,
     }
 }
